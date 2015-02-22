ed7554b4ac2527266babdb9c0b7b4f80c0b7f32d
Birkan Uzun <birkanuzun27@gmail.com>
2015-02-22 15:54:15 -0500

    Implement communication between central and peripheral

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var noble = require('noble');
 
 var rhynoConnectionUuid = '6e400001b5a3f393e0a9e50e24dcca9e';
 var rhynoPeripheralUuid = '60339882cd844530b82dadc3a303a196';
+var rhynoActionCharacteristicUuid = '6e400003b5a3f393e0a9e50e24dcca9e';
 
 var app = express();
 
@@ -31,12 +32,31 @@ noble.on('discover', function(peripheral) {
     noble.stopScanning();
 
     peripheral.on('disconnect', function() {
-        process.exit(0);
-        noble.startScanning([rhynoConnectionUuid], false);
+      process.exit(0);
+      noble.startScanning([rhynoConnectionUuid], false);
     });
 
     peripheral.connect(function(err) {
-        console.log('Connected to peripheral with uuid: ', peripheral.uuid);
+      console.log('Connected to peripheral with uuid: ', peripheral.uuid);
+      peripheral.discoverAllServicesAndCharacteristics(function(err, services, characteristics) {
+        characteristics.forEach(function(characteristic) {
+          if (characteristic.uuid == rhynoActionCharacteristicUuid) {
+            console.log('Found Action Characteristic.');
+            characteristic.on('read', function(data, isNotification) {
+              if (data.length === 1) {
+                var result = data.readUInt8(0);
+                console.log("Received message from peripheral: ", result);
+              }
+              else {
+                console.log('Received data length is incorrect.');
+              }
+            });
+            characteristic.notify(true, function(error) {
+              console.log('Action Characteristic notification is on.');
+            });
+          }
+        });
+      });
     });
   }
 });
