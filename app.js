707f755ace8861d4c6cb4311ae41c5c16b5ebabf
Birkan Uzun <birkanuzun27@gmail.com>
2015-06-06 13:41:17 -0400

    Commented out socket.io code

--- app.js
+++ app.js
@@ -16,23 +16,24 @@ var server = http.listen(3000, function () {
   console.log('\nApp listening at http://%s:%s. \n', host, port);
 });
 
-var rhynoConnectionUuid = '6e400001b5a3f393e0a9e50e24dcca9e',
-    rhynoPeripheralUuid = '60339882cd844530b82dadc3a303a196',
-    rhynoActionCharacteristicUuid = '6e400003b5a3f393e0a9e50e24dcca9e';
+var rhynoConnectionUuid = '713d0000503e4c75ba943148f18d941e',
+    rhynoPeripheralUuid = '1810ed4ffa5e4c509e51705390a4217d',
+    rhynoActionCharacteristicUuid = '713d0002503e4c75ba943148f18d941e';
 
-var ioClientConnected = false;
-io.on('connection', function(socket){
-  console.log('Connected to 2048 Client via Socket.io. \n');
-  ioClientConnected = true;
-  socket.on('disconnect', function(){
-    console.log('2048 Client disconnected. \n');
-    ioClientConnected = false;
-  });
-});
+// var ioClientConnected = false;
+// io.on('connection', function(socket){
+//   console.log('Connected to 2048 Client via Socket.io. \n');
+//   ioClientConnected = true;
+//   socket.on('disconnect', function(){
+//     console.log('2048 Client disconnected. \n');
+//     ioClientConnected = false;
+//   });
+// });
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
     console.log('Started scanning for peripheral... \n');
+    //noble.startScanning([], true);
     noble.startScanning([rhynoConnectionUuid], false);
   } else {
     console.log('Not scanning for peripheral... \n');
@@ -47,6 +48,7 @@ noble.on('discover', function(peripheral) {
 
     peripheral.on('disconnect', function() {
       process.exit(0);
+      //noble.startScanning([], true);
       noble.startScanning([rhynoConnectionUuid], false);
     });
 
@@ -57,43 +59,45 @@ noble.on('discover', function(peripheral) {
           if (characteristic.uuid == rhynoActionCharacteristicUuid) {
             console.log('Found Action Characteristic. \n');
             characteristic.on('read', function(data, isNotification) {
-              if (data.length === 1 && ioClientConnected) {
+              // if (data && ioClientConnected) {
+              if (data) {
                 var result = data.readUInt8(0);
-                switch (result) {
-                  case 117:
-                    console.log('Received Action from Rhyno: UP. \n');
-                    io.emit('action', 'up');
-                    break;
-                  case 100: 
-                    console.log('Received Action from Rhyno: DOWN. \n');
-                    io.emit('action', 'down');
-                    break;
-                  case 108: 
-                    console.log('Received Action from Rhyno: LEFT. \n');
-                    io.emit('action', 'left');
-                    break; 
-                  case 114:
-                    console.log('Received Action from Rhyno: RIGHT. \n');
-                    io.emit('action', 'right');
-                    break;
-                  case 105:
-                    console.log('Received Action from Rhyno: ZOOM IN. \n');
-                    io.emit('action', 'zoom in');
-                    break;
-                  case 111:
-                    console.log('Received Action from Rhyno: ZOOM OUT. \n');
-                    io.emit('action', 'zoom out');
-                    break;
-                  default:
-                    console.log('Data received from Rhyno is not a valid action. \n');
-                }
+                console.log("Data Received: " + result);
+                // switch (result) {
+                //   case 117:
+                //     console.log('Received Action from Rhyno: UP. \n');
+                //     io.emit('action', 'up');
+                //     break;
+                //   case 100: 
+                //     console.log('Received Action from Rhyno: DOWN. \n');
+                //     io.emit('action', 'down');
+                //     break;
+                //   case 108: 
+                //     console.log('Received Action from Rhyno: LEFT. \n');
+                //     io.emit('action', 'left');
+                //     break; 
+                //   case 114:
+                //     console.log('Received Action from Rhyno: RIGHT. \n');
+                //     io.emit('action', 'right');
+                //     break;
+                //   case 105:
+                //     console.log('Received Action from Rhyno: ZOOM IN. \n');
+                //     io.emit('action', 'zoom in');
+                //     break;
+                //   case 111:
+                //     console.log('Received Action from Rhyno: ZOOM OUT. \n');
+                //     io.emit('action', 'zoom out');
+                //     break;
+                //   default:
+                //     console.log('Data received from Rhyno is not a valid action. \n');
+                // }
               }
               else {
                 console.log('Data received from Rhyno has an incorrect length \n');
               }
             });
             characteristic.notify(true, function(error) {
-              console.log('Action Characteristic notification is on. \n');
+              console.log('Action Characteristic notification is on. Listening for action commands...\n');
             });
           }
         });
