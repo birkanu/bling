39588badb0d6562c6a1e18b4cc5b23ff9fe0a9a8
Birkan Uzun <birkanuzun27@gmail.com>
2015-06-11 19:25:13 -0400

    Implement connecting to multiple peripherals

--- bling-connection-js/app.js
+++ bling-connection-js/app.js
@@ -2,23 +2,15 @@ var express = require('express'),
     app = express(),
     http = require('http').Server(app),
     io = require('socket.io')(http),
-    noble = require('noble');
+    noble = require('noble'),
+    HashMap = require('hashmap');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+var BLING_UART_SERVICE_UUID = '713d0000503e4c75ba943148f18d941e'; // Custom Bling UART Service UUID
+var BLING_ACTION_CHAR_UUID = '713d0002503e4c75ba943148f18d941e'; // Custom Bling Action Characteristic UUID (Properties: Read, Notify)
 
-app.use(express.static(__dirname + '/public'));
+var peripheralMap = new HashMap(); // HashMap to store peripherals that are connected
 
-var server = http.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('\nApp listening at http://%s:%s. \n', host, port);
-});
-
-var blingConnectionUuid = '713d0000503e4c75ba943148f18d941e',
-    blingPeripheralUuid = '1810ed4ffa5e4c509e51705390a4217d',
-    blingActionCharacteristicUuid = '713d0002503e4c75ba943148f18d941e';
+var MAX_PERIPHERALS = 6;
 
 // var ioClientConnected = false;
 // io.on('connection', function(socket){
@@ -30,64 +22,105 @@ var blingConnectionUuid = '713d0000503e4c75ba943148f18d941e',
 //   });
 // });
 
+// Represents a peripheral in JSON format
+var peripheralToJson = function (peripheral) {
+  return {
+    "state": peripheral.state,
+    "uuid": peripheral.uuid,
+    "address": peripheral.address,
+    "advertisement": peripheral.advertisement,
+    "rssi": peripheral.rssi,
+    "services": peripheral.services
+  };
+};
+
+// Represents action data received from a peripheral in JSON format
+var actionDataToJson = function (data) {
+  return {
+    "acceleration": {
+      "x": data.readInt16BE(0) / 16324.0,
+      "y": data.readInt16BE(2) / 16324.0,
+      "z": data.readInt16BE(4) / 16324.0
+    }, 
+    "gyroscope": {
+      "x": data.readInt16BE(8) / 131.0,
+      "y": data.readInt16BE(10) / 131.0,
+      "z": data.readInt16BE(12) / 131.0   
+    },
+    "timestamp": Date.now()
+  };
+};
+
+// Starts connecting to a peripheral once it is discovered
+var discover = function (peripheral) {
+  console.log('Discovered peripheral:\n', peripheralToJson(peripheral), '\n');
+  if (!peripheralMap.has(peripheral.uuid)) {
+    peripheral.connect(connect.bind(peripheral));
+  }
+};
+
+// Starts discovering a peripheral's services and characteristics once it is connected
+var connect = function (error) {
+  if (error) {
+    throw error;
+  } else {
+    console.log('Connected to peripheral with uuid: ', this.uuid, '\n');
+    peripheralMap.set(this.uuid, this);
+    // If the maximum peripheral count to be connected is exceeded, stop scanning
+    if (peripheralMap.count() >= MAX_PERIPHERALS) {
+      console.log("Stopped scanning for peripherals: Reached " + MAX_PERIPHERALS + " peripherals.");
+      noble.stopScanning();
+    }
+    // If the peripheral disconnects, remove it from the peripheral HashMap, and restart scanning
+    this.on('disconnect', function() {
+      console.log('Peripheral with uuid, ', this.uuid, ', disconnected. \n');
+      peripheralMap.remove(this.uuid);
+      if (peripheralMap.count() < MAX_PERIPHERALS) {
+        console.log('Started scanning for peripherals... \n');
+        noble.startScanning([BLING_UART_SERVICE_UUID], false);
+      }
+    });
+    this.discoverSomeServicesAndCharacteristics(
+      [BLING_UART_SERVICE_UUID], 
+      [BLING_ACTION_CHAR_UUID], 
+      discoverSomeServicesAndCharacteristics.bind(this)
+    );
+  }
+};
+
+var discoverSomeServicesAndCharacteristics = function (error, services, characteristics) {
+  if (error) {
+    throw error;
+  } else {
+    var blingActionChar;
+    characteristics.forEach(function(characteristic) {
+      if (characteristic.uuid == BLING_ACTION_CHAR_UUID) {
+        console.log('Found Action Characteristic for peripheral. \n');
+        blingActionChar = characteristic;
+      }
+      blingActionChar.notify(true, function(error) {
+        console.log('Action Characteristic notifications for peripheral are on.\n');
+      });
+      blingActionChar.on('read', function(data, isNotification) {
+        if (data) {
+          var actionData = actionDataToJson(data);
+          console.log("Data received from ", this.uuid, ":\n", actionData, "\n");
+        }
+      });
+    });
+  }
+};
+
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
-    console.log('Started scanning for peripheral... \n');
-    //noble.startScanning([], true);
-    noble.startScanning([blingConnectionUuid], false);
+    // The BLE radio has been powered on. Begin scanning for peripherals
+    // that provide the Custom Bling UART Service.
+    console.log('Started scanning for peripherals... \n');
+    noble.startScanning([BLING_UART_SERVICE_UUID], false);
   } else {
-    console.log('Not scanning for peripheral... \n');
+    console.log('BLE radio is not powered on. Not scanning for peripherals... \n');
     noble.stopScanning();
   }
 });
 
-noble.on('discover', function(peripheral) {
-  console.log('Discovered peripheral: ', peripheral.advertisement, '\n');
-  // if (peripheral.uuid === blingPeripheralUuid) {
-    noble.stopScanning();
-
-    peripheral.on('disconnect', function() {
-      process.exit(0);
-      //noble.startScanning([], true);
-      noble.startScanning([blingConnectionUuid], false);
-    });
-
-    peripheral.connect(function(err) {
-      console.log('Connected to peripheral with uuid: ', peripheral.uuid, '\n');
-      peripheral.discoverAllServicesAndCharacteristics(function(err, services, characteristics) {
-        characteristics.forEach(function(characteristic) {
-          if (characteristic.uuid == blingActionCharacteristicUuid) {
-            console.log('Found Action Characteristic. \n');
-            characteristic.on('read', function(data, isNotification) {
-              // if (data && ioClientConnected) {
-              if (data) {
-                var resultAccX = data.readInt16BE(0);
-                var resultAccY = data.readInt16BE(2);
-                var resultAccZ = data.readInt16BE(4);
-                var resultTemp = data.readInt16BE(6);
-                var resultGyrX = data.readInt16BE(8);
-                var resultGyrY = data.readInt16BE(10);
-                var resultGyrZ = data.readInt16BE(12);
-                console.log("Acc_X Received: " + resultAccX/16324.0 + " G");
-                console.log("Acc_Y Received: " + resultAccY/16324.0 + " G");
-                console.log("Acc_Z Received: " + resultAccZ/16324.0 + " G");
-                console.log("Temp Received: " + resultTemp/340.0+36.53 + " Celcius");
-                console.log("Gyr_X Received: " + resultGyrX/131.0 + " DPS");
-                console.log("Gyr_Y Received: " + resultGyrY/131.0 + " DPS");
-                console.log("Gyr_Z Received: " + resultGyrZ/131.0 + " DPS");
-                console.log("----------------------------------------------");
-                console.log("----------------------------------------------");
-              }
-              else {
-                console.log('Data received from Bling has an incorrect length \n');
-              }
-            });
-            characteristic.notify(true, function(error) {
-              console.log('Action Characteristic notification is on. Listening for action commands...\n');
-            });
-          }
-        });
-      });
-    });
-  // }
-});
+noble.on('discover', discover);
